7983d047dd40778e3cef32de45ccb9530b26d9ec
Max Shu <shushakov@gmail.com>
2025-07-23 16:09:48 +0800

    cleanup + userscript

---
+++ portal.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name        Админка фикс
+// @namespace   Violentmonkey Scripts
+// @match       https://www.hse.ru/adm/*
+// @grant       GM_addStyle
+// @version     1.1
+// @author      kotkota
+// @description Делает админку портала Вышки более дружелюбной.
+// @run-at      document-idle
+// @homepageURL    https://github.com/kotkota/hse-portal
+// @updateURL      https://raw.githubusercontent.com/kotkota/hse-portal/master/portal.user.js
+// @downloadURL    https://raw.githubusercontent.com/kotkota/hse-portal/master/portal.user.js
+// ==/UserScript==
+
+
+(function() {
+  'use strict';
+
+    function adjustTextareaRows(textareaElement) {
+
+        const textValue = textareaElement.value;
+        const numberOfNewlines = (textValue.match(/\n/g) || ['']).length;
+
+        if (numberOfNewlines > 10) {
+          textareaElement.setAttribute('rows', numberOfNewlines);
+        }
+
+    }
+  setTimeout(() => {
+
+    const textAreas = document.querySelectorAll('textarea.fullWidth');
+
+    textAreas.forEach(textarea => {
+
+        adjustTextareaRows(textarea);
+        textarea.addEventListener('input', () => adjustTextareaRows(textarea));
+    });
+  }, 3000);
+
+  const css = `
+    .container > div > .__widget_wrapper > .__widget_builder_destination,
+    .__widget_builder_destination.__widget_builder_preview,
+    .__widget_wrapper textarea {
+      height: max-content !important;
+    }
+  `;
+
+  GM_addStyle(css);
+})();
