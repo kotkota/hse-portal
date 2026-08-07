fb1815d10fa0b6594d7ca0217b03d1f3940b5417
Max Shu <shushakov@gmail.com>
2026-08-08 00:13:21 +0800

    font updates

--- portal.user.js
+++ portal.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.hse.ru/adm/*
 // @grant       GM_addStyle
-// @version     1.25
+// @version     1.26
 // @author      kotkota
 // @description Делает админку портала Вышки более дружелюбной.
 // @run-at      document-idle
@@ -16,9 +16,85 @@
 (function() {
   'use strict';
 
+  function adjustTextareaRows(textareaElement) {
+
+      const textValue = textareaElement.value;
+      const numberOfNewlines = (textValue.match(/\n/g) || ['']).length;
+
+      if (numberOfNewlines > 10) {
+        textareaElement.setAttribute('rows', numberOfNewlines);
+      }
+
+  }
+
+  setTimeout(() => {
+
+    const codeInsertHeadings = Array.from(document.querySelectorAll('.panel-heading')).filter(heading => {
+      return heading.textContent.includes('Кодовая вставка');
+    });
+
+    codeInsertHeadings.forEach(textarea => {
+        textarea.nextElementSibling.classList.add('monospaced');
+    });
+
+    const builderBlocks = document.querySelector('.__widget_builder_source_outer');
+    if (builderBlocks) {
+      builderBlocks.prepend( document.querySelector('.__widget_builder_fold') );
+      builderBlocks.prepend( document.querySelector('.__widget_builder_maximize') );
+    }
+
+  }, 3000);
+
   const css = `
-    .__widget_wrapper textarea {
-      field-sizing: content;;
+    @font-face {
+      font-family: 'HSE Sans';
+      src: url(/f/src/global/fonts/HSESans-Regular.woff2) format("woff2"), url(/f/src/global/fonts/HSESans-Regular.woff) format("woff");
+      font-weight: 400;
+      font-style: normal
+    }
+    @font-face {
+      font-family: 'HSE Sans';
+      src: url(/f/src/global/fonts/HSESans-SemiBold.woff2) format("woff2"), url(/f/src/global/fonts/HSESans-SemiBold.woff) format("woff");
+      font-weight: 600;
+      font-style: normal
+    }
+    @font-face {
+      font-family: 'HSE Sans';
+      src: url(/f/src/global/fonts/HSESans-Bold.woff2) format("woff2"), url(/f/src/global/fonts/HSESans-Bold.woff) format("woff");
+      font-weight: 700;
+      font-style: normal
+    }
+    @font-face {
+      font-family: 'HSE Sans';
+      src: url(/f/src/global/fonts/HSESans-Black.woff2) format("woff2"), url(/f/src/global/fonts/HSESans-Black.woff) format("woff");
+      font-weight: 900;
+      font-style: normal
+    }
+    body, td {
+      font-family: 'HSE Sans', 'Helvetica Neue', tahoma, sans-serif;
+      font-size: 14px;
+    }
+    .monospaced {font-family: monospace;}
+    div {font-size: revert;}
+    .__widget_wrapper textarea {field-sizing: content;}
+    label, b, strong {font-weight: 600;}
+    .panel-heading {
+      font-size: 16px;
+      padding: .5em .75em;
+    }
+    .cke_editable {font-size: 16px;}
+
+    input.ui-button {
+      font-family: 'HSE Sans', 'Helvetica Neue', tahoma, sans-serif;
+      text-transform: uppercase;
+      padding: .5em .75em;
+      font-weight: 600;
+      letter-spacing: .05em;
+      border-radius: 5px;
+    }
+    .__widget_builder_source.ui-sortable {
+      box-sizing: border-box;
+      height: 100svh;
     }
     .__widget_builder_destination.__widget_builder_preview.__widget_builder_destination_only {
       height: revert;
